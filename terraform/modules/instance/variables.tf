8301e1377f68047c72d698048d4bc1fb1da028e4
Sergey_Shchukin <oerby123@gmail.com>
2023-05-18 13:00:29 +0300

    terraform_3

--- terraform/modules/instance/variables.tf
+++ terraform/modules/instance/variables.tf
@@ -1,7 +1,6 @@
 variable "instance_family_image" {
   description = "Instance image"
   type        = string
-  my-name     = string
 }
 
 variable "vpc_subnet_id" {
@@ -11,5 +10,10 @@ variable "vpc_subnet_id" {
 
 variable "vpc_subnet_zone" {
   description = "VPC subnet network zone"
+  type        = string
+}
+
+variable "my-name" {
+  description = "name vm"
   type        = string
 }
